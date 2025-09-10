7b9a816f68c490c09c400a2da8300f4c533bfd9c
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-09-10 06:50:44 +0530

    feat: enhance TNotes component with dynamic note title input

--- client/src/page/Teacher/TNotes.tsx
+++ client/src/page/Teacher/TNotes.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 
 export default function TNotes() {
+    const [visible, setVisible] = useState(false);
   return (
     <div className="max-w-4xl mx-auto p-6">
         <Card>
@@ -20,6 +24,30 @@ export default function TNotes() {
                 </form>
             </CardContent>
         </Card>
+
+        {
+            visible && (
+                <div className="mt-6">
+                    <Card>
+                        <CardContent>
+                            <form>
+                                <div>
+                                    <Label>
+                                        Title
+                                    </Label>
+
+                                    <Input placeholder="Set the title of the notes..." className="mt-2 mb-4" />
+                                </div>
+
+                                <Button>
+                                    Save Notes
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
+            )
+        }
     </div>
   )
 }
