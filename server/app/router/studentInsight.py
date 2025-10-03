9cf3a515584e6224ba86e4af44d4283af60aaf71
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-10-03 16:16:47 +0530

    feat: enhance generate_industry_node to provide structured industry insights in JSON format

--- server/app/router/studentInsight.py
+++ server/app/router/studentInsight.py
@@ -47,4 +47,48 @@ def tavily_search_node(state: State):
 
 
 def generate_industry_node(state: State):
+    ind = state["industry"][-1].content
+    res = state["research"][-1].content if state["research"] else "No research data available."
+
+
+    prompt = (
+        f"Analyze the current state of the {ind} industry and provide insights is only the following JSON format without any additional notes or explanations:\n\n"
+        f"Research:\n{res}\n\n"
+        f'{{"salary_range": [{"role": "string", "min": number, "max": number, "median": number, "location": "string"}]}}'
+        f'{{"growth_rate": number}}'
+        f'{{"demand_level": "Hight" | "Medium" | "Low"}}'
+        f'{{"top_skills": ["string"]}}'
+        f'{{"market_outlook": "Positive" | "Neutral" | "Negative"}}'
+        f'{{"key_trends": ["string"]}}'
+        f'{{"recommend_skills": ["string"]}}'
+
+        f"IMPORTANT: Return only the JSON. No additional text or explanations, note, or makdown formatting."
+        f"Include at least 5 common roles for salary_range."
+        f"Growth rate should be a percentage value."
+        f"Include at least 5 skills and 5 key trends."
+    )
+
+    response = llm.invoke(prompt)
+
+    if not response or not response.content:
+        raise HTTPException(status_code=500, detail="Failed to generate industry insights.")
     
+    raw_output = response.content.strip()
+
+    return {
+        "industry": state["industry"],
+        "research": state["research"],
+        "getIndustry": [HumanMessage(content=raw_output)],
+    }
+
+
+workflow = StateGraph(State)
+workflow.add_node("tavily_search", tavily_search_node)
+workflow.add_node("generate_industry", generate_industry_node)
+workflow.set_entry_point("tavily_search")
+
+workflow.add_edge("tavily_search", "generate_industry")
+workflow.add_edge("generate_industry", END)
+
+graph = workflow.compile()
+
