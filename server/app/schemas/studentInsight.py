2df935d564c0d2e0a4f66d33b2ec2d09327691d6
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-10-07 23:07:32 +0530

    refactor: remove industry field from StudentInsight model for simplification

--- server/app/schemas/studentInsight.py
+++ server/app/schemas/studentInsight.py
@@ -4,8 +4,10 @@ from datetime import datetime
 from typing import Optional
 from app.schemas.auth import UserResponse
 
+
 class StudentInsightCreate(BaseModel):
     industry: str = Field(..., example="Technology")
+
 
 class SalaryRange(BaseModel):
     role: str
@@ -14,21 +16,31 @@ class SalaryRange(BaseModel):
     median: int
     location: str
 
+
 class StudentInsightBase(BaseModel):
-    salary_range:  List[SalaryRange] 
+    salary_range: List[SalaryRange]
     growth_rate: float = Field(..., example=5.4)
     demand_level: str = Field(..., example="High")
-    top_skills: List[str] = Field(..., example=["Python", "Data Analysis", "Machine Learning"])
-    market_outlook: str = Field(..., example="The job market for data scientists is expected to grow significantly over the next decade.")
-    key_trends: List[str] = Field(..., example=["Increased use of AI", "Big Data Analytics", "Cloud Computing"])
-    recommend_skills: List[str] = Field(..., example=["Deep Learning", "Statistical Analysis", "Data Visualization"])
+    top_skills: List[str] = Field(
+        ..., example=["Python", "Data Analysis", "Machine Learning"]
+    )
+    market_outlook: str = Field(
+        ...,
+        example="The job market for data scientists is expected to grow significantly over the next decade.",
+    )
+    key_trends: List[str] = Field(
+        ..., example=["Increased use of AI", "Big Data Analytics", "Cloud Computing"]
+    )
+    recommend_skills: List[str] = Field(
+        ..., example=["Deep Learning", "Statistical Analysis", "Data Visualization"]
+    )
 
 
 class StudentInsightResponse(StudentInsightBase):
     id: str
     created_at: datetime
     updated_at: datetime
-    owner: Optional[UserResponse]  
+    owner: Optional[UserResponse]
 
     class Config:
         from_attributes = True
